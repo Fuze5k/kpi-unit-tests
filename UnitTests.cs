ba68f6d88930fbec3bc2b54eda96c824d0008c65
Ярослав Григоренко <51718249+Fuze5k@users.noreply.github.com>
2021-12-23 06:04:10 +0200

    Update UnitTests.cs

--- UnitTests.cs
+++ UnitTests.cs
@@ -1,4 +1,4 @@
-﻿using HtmlAgilityPack;
+using HtmlAgilityPack;
 using NUnit.Framework;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,27 +12,24 @@ namespace KPI.UI.Tests
         [SetUp]
         public void Setup()
         {
-            _document = new HtmlWeb() { OverrideEncoding = Encoding.UTF8 }.Load("https://ru.m.wikipedia.org/wiki/%D0%92%D0%B8%D0%BA%D0%B8%D0%BF%D0%B5%D0%B4%D0%B8%D1%8F", "GET");
+            _document = new HtmlWeb() { OverrideEncoding = Encoding.UTF8 }
+            .Load("https://ru.m.wikipedia.org/wiki/%D0%92%D0%B8%D0%BA%D0%B8%D0%BF%D0%B5%D0%B4%D0%B8%D1%8F", "GET");
         }
 
         [Test]
-        public void Test1()
+        public void TestTitle()
         {
-            var expected_h1 = "Википедия";
-
-            var h1 = _document.DocumentNode.SelectSingleNode("//h1[@id='section_0']//h1");
-
-            Assert.AreEqual(expected_h1, h1?.InnerText?.Trim());
+            string expected = "Википедия";
+            string actual = _document.DocumentNode.SelectSingleNode("//h1[@id='section_0']").InnerText?.Trim();
+            Assert.AreEqual(expected, actual);
         }
 
         [Test]
-        public void Test2()
+        public void TestEditButton()
         {
-            var expected_div = "Добро пожаловать в Википедию";
-
-            var div = _document.DocumentNode.SelectSingleNode("//div[@id='description']//div");
-
-            Assert.AreEqual(expected_div, div.InnerText.Trim());
+            string expected = "Править";
+            string actual = _document.DocumentNode.SelectSingleNode("//a[@id='ca-edit']").InnerText?.Trim();
+            Assert.AreEqual(expected, actual);
         }
     }
 }
